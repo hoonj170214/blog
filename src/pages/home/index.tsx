a5d90ea6bc4a66bf8866ad747b64105199ad79a9
hoonj170214 <sh20170214@gmail.com>
2024-05-01 16:49:18 +0900

    home 수정

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,16 @@
 export default function Home() {
-  return <div>
-    <header>
-      <div>
-        
-      </div>
-      
-    </header>
-    <main></main>
-    <footer></footer>
-  
-  </div>
+  return (
+    <div>
+      <header>
+        <div>
+          <ul>
+            <li>글쓰기</li>
+            <li>프로필</li>
+          </ul>
+        </div>
+      </header>
+      <main></main>
+      <footer></footer>
+    </div>
+  );
 }
