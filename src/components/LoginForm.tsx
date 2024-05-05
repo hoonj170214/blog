64922804761987a4350eccc3f55a987e5bdca75f
hoonj170214 <sh20170214@gmail.com>
2024-05-05 14:52:16 +0900

    feat: 로그인폼 제작

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,26 +1,97 @@
 import { Link } from 'react-router-dom';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { toast } from 'react-toastify';
+import { app } from 'firebaseApp';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function LoginForm() {
-	return (
-		<form action="/post" method="post" className="form form-lg">
-			<h1 className="form__title">로그인</h1>
-			<div className="form__block">
-				<label htmlFor="email">이메일</label>
-				<input type="email" name="email" id="email" required />
-			</div>
-			<div className="form__block">
-				<label htmlFor="password">비밀번호</label>
-				<input type="password" name="password" id="password" required />
-			</div>
-			<div className="form__block">
-				계정이 없으신가요?{' '}
-				<Link to="/signup" className="form__link">
-					회원가입하기
-				</Link>
-			</div>
-			<div className="form__block">
-				<input type="submit" value="로그인" className="form__btn--submit" />
-			</div>
-		</form>
-	);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [error, setError] = useState<string>('');
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth(app);
+      await signInWithEmailAndPassword(auth, email, password);
+      toast.success('로그인에 성공했습니다.');
+    } catch (error: any) {
+      console.log(error);
+      toast.error('로그인에 실패했습니다.', error?.code);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === 'email') {
+      setEmail(value);
+      const validRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+      // value값이 null이 아닌지 확인하고, 사용해야 하기 때문에 널병합 연산자를 사용한다.
+      if (!value?.match(validRegex)) {
+        setError('이메일 형식이 올바르지 않습니다.');
+      } else {
+        setError('');
+      }
+    }
+    // name은 HTML에서 정해준 값이어야 하므로, 따옴표로 감싸야 한다.
+    if (name === 'password') {
+      setPassword(value);
+      if (value?.length < 8) {
+        setError('비밀번호는 8자리 이상이어야 합니다.');
+      } else {
+        setError('');
+      }
+    }
+  };
+
+  return (
+    // action, method 대신에 onSubmit 으로 핸들러를 넘겨주면 된다.
+    <form onSubmit={handleSubmit} className="form form-lg">
+      <h1 className="form__title">로그인</h1>
+      <div className="form__block">
+        <label htmlFor="email">이메일</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          required
+          onChange={handleChange}
+          value={email}
+        />
+      </div>
+      <div className="form__block">
+        <label htmlFor="password">비밀번호</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          required
+          onChange={handleChange}
+          value={password}
+        />
+      </div>
+      {error && error?.length > 0 && (
+        <div className="form__block">
+          <div className="form__error">{error}</div>
+        </div>
+      )}
+      <div className="form__block">
+        계정이 없으신가요?{' '}
+        <Link to="/signup" className="form__link">
+          회원가입하기
+        </Link>
+      </div>
+      <div className="form__block">
+        <input
+          type="submit"
+          value="로그인"
+          className="form__btn--submit"
+          disabled={error?.length > 0}
+        />
+      </div>
+    </form>
+  );
 }
