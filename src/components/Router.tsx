c754fbd7cd420947da86607fd14cc9c9acd032a4
hoonj170214 <sh20170214@gmail.com>
2024-05-05 12:00:56 +0900

    fix: 라우터에서 인증정보를 받지 않고, 앱 컴포넌트에서 받도록 수정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from 'pages/home';
 import PostList from 'pages/posts';
@@ -9,32 +8,34 @@ import ProfilePage from 'pages/profile';
 import LoginPage from 'pages/login';
 import SignupPage from 'pages/signup';
 
-export default function Router() {
-	// firebase Auth가 인증되었으면 true로 변경해주는 로직 추가
-	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-	return (
-		<>
-			<Routes>
-				{isAuthenticated ? (
-					<>
-						<Route path="/" element={<Home />} />
-						<Route path="/posts" element={<PostList />} />
-						<Route path="/posts/:id" element={<PostDetail />} />
-						<Route path="/posts/new" element={<PostNew />} />
-						<Route path="/posts/edit/:id" element={<PostEdit />} />
-						<Route path="/profile" element={<ProfilePage />} />
-						<Route path="/login" element={<LoginPage />} />
-						<Route path="/signup" element={<SignupPage />} />
-						<Route path="*" element={<Navigate replace to="/" />} />
-					</>
-				) : (
-					<>
-						<Route path="/login" element={<LoginPage />} />
-						<Route path="/signup" element={<SignupPage />} />
-						<Route path="*" element={<LoginPage />} />
-					</>
-				)}
-			</Routes>
-		</>
-	);
+interface RouterProps {
+  isAuthenticated: boolean;
+}
+
+export default function Router({ isAuthenticated }: RouterProps) {
+  return (
+    <>
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/posts" element={<PostList />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+            <Route path="/posts/new" element={<PostNew />} />
+            <Route path="/posts/edit/:id" element={<PostEdit />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<Navigate replace to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="*" element={<LoginPage />} />
+          </>
+        )}
+      </Routes>
+    </>
+  );
 }
